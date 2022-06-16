4860051b1efaf9f13b945ec179f97ae88d35cc6d
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 21:08:22 +0100

    Add files via upload

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+document.addEventListner('DOMContentLoaded', (event) => {
+    const amenityIDlist = [];
+    const amenityDict = {};
+    $('input:checkbox').click(function (){
+	if $(this).is(':checked')){
+	amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+	amenityDict = Object.keys(amenityDict);
+	const vals = Object.values(amenityDict);
+	$('DIV.amenities h4').append(vals);
+    } else{
+	delete amenityDict[$(this).attr('data-id')];
+    }
+			     })
+			});
